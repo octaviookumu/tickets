fa2cdc97eee76c53c67b5438347c3fa1699ff558
octaviookumu <octaviookumu@gmail.com>
2026-02-14 15:14:24 +0300

    feat: implement User entity

---
+++ src/main/java/com/octaviookumu/tickets/domain/User.java
@@ -0,0 +1,52 @@
+package com.octaviookumu.tickets.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+// problem with using @Data for the case of bidirectional relationships
+// using @Data could lead to a stack overflow exception
+// it is better to specify the annotations we need rather than use @Data
+// and then manually implement our equals and hashcode methods
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    // TODO: Organized events
+
+    // TODO: Attending events
+
+    // TODO: Staffing events
+
+    // to know when an entity has been created and updated
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
