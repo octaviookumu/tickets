e943083e8d6f4729e193113169a39e72932389e2
octaviookumu <octaviookumu@gmail.com>
2026-02-14 15:52:58 +0300

    feat: implement Event entity

---
+++ src/main/java/com/octaviookumu/tickets/domain/Event.java
@@ -0,0 +1,71 @@
+package com.octaviookumu.tickets.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id; // not in keycloak. will be generated
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "start")
+    private LocalDate start;
+
+    @Column(name = "end")
+    private LocalDate end;
+
+    @Column(name = "venue", nullable = false)
+    private String venue;
+
+    // when the event tickets go on sale
+    @Column(name = "sales_start")
+    private LocalDate salesStart;
+
+    @Column(name = "sales_end")
+    private LocalDate salesEnd;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING) // defaults to string (representation of the enum will be stored in the db)
+    private EventStatusEnum status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    // organizer can organize many events. LAZY is for efficiency reasons (helps combat the 'n+1 problem')
+    @JoinColumn(name = "organizer_id") // will reference the id of the organizer, which is a user
+    private User organizer;
+
+    // not putting the configuration/ all the info inside the event
+    // it makes sense for the user to own this relationship
+    @ManyToMany(mappedBy = "attendingEvents") // specify the name of the instance variable on the User class
+    private List<User> attendees = new ArrayList<>(); // helps prevent nullPointerExceptions
+
+    @ManyToMany(mappedBy = "staffingEvents")
+    private List<User> staff = new ArrayList<>();
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
