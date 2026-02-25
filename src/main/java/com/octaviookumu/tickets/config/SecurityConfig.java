6998643e44bf766a85c92925b275f96c8cc2aec4
octaviookumu <octaviookumu@gmail.com>
2026-02-25 11:16:26 +0300

    feat: configure spring security to use the user filter - configure security SecurityFilterChain

---
+++ src/main/java/com/octaviookumu/tickets/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.octaviookumu.tickets.config;
+
+import com.octaviookumu.tickets.filters.UserProvisioningFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    private SecurityFilterChain filterChain(
+            HttpSecurity http,
+            UserProvisioningFilter userProvisioningFilter) throws Exception {
+        http.authorizeHttpRequests(authorize ->
+                        // catch all rule. every request will be authenticated by default
+                        authorize.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()))
+                .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
