c09f138994745e550cb8870a9888da52c7198933
octaviookumu <octaviookumu@gmail.com>
2026-03-04 11:48:13 +0300

    refactor: remove organizer from CreateEventRequest

--- src/main/java/com/octaviookumu/tickets/domain/CreateEventRequest.java
+++ src/main/java/com/octaviookumu/tickets/domain/CreateEventRequest.java
@@ -1,7 +1,6 @@
 package com.octaviookumu.tickets.domain;
 
 import com.octaviookumu.tickets.domain.entities.EventStatusEnum;
-import com.octaviookumu.tickets.domain.entities.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,7 +21,5 @@ public class CreateEventRequest {
     private LocalDateTime salesStart;
     private LocalDateTime salesEnd;
     private EventStatusEnum status;
-    private User organizer; // could pass something similar to CreateTicketTypeRequest
-    // but no need. As the user will be passed in from the user. ie The caller of the REST API
     private List<CreateTicketTypeRequest> ticketTypes = new ArrayList<>();
 }
