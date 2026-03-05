17d0e3d191143b964d4dfcda3314f754eeaac141
octaviookumu <octaviookumu@gmail.com>
2026-03-05 11:15:06 +0300

    feat: create EventController class and create event endpoint

---
+++ src/main/java/com/octaviookumu/tickets/domain/controllers/EventController.java
@@ -0,0 +1,51 @@
+package com.octaviookumu.tickets.domain.controllers;
+
+import com.octaviookumu.tickets.domain.CreateEventRequest;
+import com.octaviookumu.tickets.domain.dtos.CreateEventRequestDto;
+import com.octaviookumu.tickets.domain.dtos.CreateEventResponseDto;
+import com.octaviookumu.tickets.domain.entities.Event;
+import com.octaviookumu.tickets.mappers.EventMapper;
+import com.octaviookumu.tickets.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/v1/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventMapper eventMapper;
+    private final EventService eventService;
+
+
+    /**
+     * @param jwt                   JWT token passed by the caller
+     * @param createEventRequestDto request payload
+     *                              <p>
+     *                              The DTO is converted to a service-layer request using
+     *                              {@code eventMapper.fromDto(createEventRequestDto)}.
+     * @return response
+     */
+    @PostMapping
+    ResponseEntity<CreateEventResponseDto> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @Valid @RequestBody CreateEventRequestDto createEventRequestDto
+    ) {
+        CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
+        UUID userId = UUID.fromString(jwt.getSubject());
+        Event createdEvent = eventService.createEvent(userId, createEventRequest);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createdEvent);
+        return new ResponseEntity<>(createEventResponseDto, HttpStatus.CREATED);
+    }
+
+}
