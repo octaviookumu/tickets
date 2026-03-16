18a2fea31c9f4523e056b7bfcacda792208e215a
octaviookumu <octaviookumu@gmail.com>
2026-03-16 23:12:25 +0300

    feat: add repository method to find events by id and organizer id

--- backend/src/main/java/com/octaviookumu/tickets/services/EventService.java
+++ backend/src/main/java/com/octaviookumu/tickets/services/EventService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -15,4 +16,6 @@ public interface EventService {
     Event createEvent(UUID organizerId, CreateEventRequest event);
 
     Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable);
+
+    Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
 }
