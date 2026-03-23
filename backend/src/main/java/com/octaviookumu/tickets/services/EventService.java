ae61ea3124d2cce9ab7540c4793d65397cd44f6d
octaviookumu <octaviookumu@gmail.com>
2026-03-23 11:32:25 +0300

    feat: add UpdateEventRequest and UpdateTicketTypeRequest

--- backend/src/main/java/com/octaviookumu/tickets/services/EventService.java
+++ backend/src/main/java/com/octaviookumu/tickets/services/EventService.java
@@ -1,6 +1,7 @@
 package com.octaviookumu.tickets.services;
 
 import com.octaviookumu.tickets.domain.CreateEventRequest;
+import com.octaviookumu.tickets.domain.UpdateEventRequest;
 import com.octaviookumu.tickets.domain.entities.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,4 +19,6 @@ public interface EventService {
     Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable);
 
     Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
+
+    Event updateEventForOrganizer(UUID organizerId, UUID id, UpdateEventRequest event);
 }
