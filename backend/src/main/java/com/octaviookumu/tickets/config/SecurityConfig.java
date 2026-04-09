b01a46f9a6c37de991194e205ea3c6716bd6e701
octaviookumu <octaviookumu@gmail.com>
2026-04-09 14:53:21 +0300

    feat: implement getPublishedEvent endpoint - update SecurityConfig to make published-events a public api

--- backend/src/main/java/com/octaviookumu/tickets/config/SecurityConfig.java
+++ backend/src/main/java/com/octaviookumu/tickets/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
             UserProvisioningFilter userProvisioningFilter) throws Exception {
         http.authorizeHttpRequests(authorize ->
                         authorize
-                                .requestMatchers(HttpMethod.GET, "/api/v1/published-events").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll()
                                 // catch all rule. every request will be authenticated by default
                                 .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
--- backend/src/main/java/com/octaviookumu/tickets/domain/controllers/PublishedEventController.java
+++ backend/src/main/java/com/octaviookumu/tickets/domain/controllers/PublishedEventController.java
@@ -1,5 +1,6 @@
 package com.octaviookumu.tickets.domain.controllers;
 
+import com.octaviookumu.tickets.domain.dtos.GetPublishedEventDetailsResponseDto;
 import com.octaviookumu.tickets.domain.dtos.ListPublishedEventResponseDto;
 import com.octaviookumu.tickets.domain.entities.Event;
 import com.octaviookumu.tickets.mappers.EventMapper;
@@ -7,11 +8,13 @@ import com.octaviookumu.tickets.services.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "api/v1/published-events")
@@ -36,5 +39,15 @@ public class PublishedEventController {
         return ResponseEntity.ok(
                 events.map(eventMapper::toListPublishedEventResponseDto)
         ); // returns a 200
+    }
+
+    @GetMapping(path = "/{eventId}")
+    public ResponseEntity<GetPublishedEventDetailsResponseDto> getPublishedEvent(
+            @PathVariable UUID eventId
+    ) {
+        return eventService.getPublishedEvent(eventId)
+                .map(eventMapper::toGetPublishedEventDetailsResponseDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
