68d5cf38c8e3198fa08575d93eab9c944b8fb560
octaviookumu <octaviookumu@gmail.com>
2026-04-23 17:09:52 +0300

    feat: extract roles - add a custom JWT converter to extract a user's roles - update SecurityConfig to use JWT converter

--- backend/src/main/java/com/octaviookumu/tickets/config/SecurityConfig.java
+++ backend/src/main/java/com/octaviookumu/tickets/config/SecurityConfig.java
@@ -17,7 +17,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(
             HttpSecurity http,
-            UserProvisioningFilter userProvisioningFilter) throws Exception {
+            UserProvisioningFilter userProvisioningFilter,
+            JwtAuthenticationConverter jwtAuthenticationConverter
+    ) throws Exception {
         http.authorizeHttpRequests(authorize ->
                         authorize
                                 .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll()
@@ -27,7 +29,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(Customizer.withDefaults()))
+                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
                 .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
 
         return http.build();
