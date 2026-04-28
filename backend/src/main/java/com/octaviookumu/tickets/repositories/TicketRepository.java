550aa5676245f3318993346b0129e706e30c3a25
octaviookumu <octaviookumu@gmail.com>
2026-04-28 15:14:17 +0300

    feat: add findByPurchaserId method to TicketRepository - implement TicketService

--- backend/src/main/java/com/octaviookumu/tickets/repositories/TicketRepository.java
+++ backend/src/main/java/com/octaviookumu/tickets/repositories/TicketRepository.java
@@ -1,6 +1,8 @@
 package com.octaviookumu.tickets.repositories;
 
 import com.octaviookumu.tickets.domain.entities.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import java.util.UUID;
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
     int countByTicketTypeId(UUID ticketTypeId); // JPA will work out a query for this
+
+    Page<Ticket> findByPurchaserId(UUID purchaserId, Pageable pageable);
 }
---
+++ backend/src/main/java/com/octaviookumu/tickets/services/TicketService.java
@@ -0,0 +1,11 @@
+package com.octaviookumu.tickets.services;
+
+import com.octaviookumu.tickets.domain.entities.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface TicketService {
+    Page<Ticket> listTicketsForUser(UUID userId, Pageable pageable);
+}
