c2f33027465f23bef598b4e1b46ddcb8701365e2
octaviookumu <octaviookumu@gmail.com>
2026-05-12 14:25:23 +0300

    feat: add get ticket info to repository and service layers

--- backend/src/main/java/com/octaviookumu/tickets/repositories/TicketRepository.java
+++ backend/src/main/java/com/octaviookumu/tickets/repositories/TicketRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -13,4 +14,6 @@ public interface TicketRepository extends JpaRepository<Ticket, UUID> {
     int countByTicketTypeId(UUID ticketTypeId); // JPA will work out a query for this
 
     Page<Ticket> findByPurchaserId(UUID purchaserId, Pageable pageable);
+
+    Optional<Ticket> findByIdAndPurchaserId(UUID id, UUID purchaserId);
 }
