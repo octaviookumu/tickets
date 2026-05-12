06e2ff8e1c8470b5cb76bd7e911b29a13ee38708
octaviookumu <octaviookumu@gmail.com>
2026-05-12 14:55:44 +0300

    feat: implement getTicket endpoint

--- backend/src/main/java/com/octaviookumu/tickets/domain/controllers/TicketController.java
+++ backend/src/main/java/com/octaviookumu/tickets/domain/controllers/TicketController.java
@@ -1,16 +1,21 @@
 package com.octaviookumu.tickets.domain.controllers;
 
+import com.octaviookumu.tickets.domain.dtos.GetTicketResponseDto;
 import com.octaviookumu.tickets.domain.dtos.ListTicketResponseDto;
 import com.octaviookumu.tickets.mappers.TicketMapper;
 import com.octaviookumu.tickets.services.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 import static com.octaviookumu.tickets.util.JwtUtil.parseUserId;
 
@@ -31,6 +36,25 @@ public class TicketController {
                 parseUserId(jwt),
                 pageable
         ).map(ticketMapper::toListTicketResponseDto);
+    }
+
+    @GetMapping("/{ticketId}")
+    public ResponseEntity<GetTicketResponseDto> getTicket(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable UUID ticketId
+    ) {
+        return ticketService
+                .getTicketForUser(parseUserId(jwt), ticketId)
+                .map(ticketMapper::toGetTicketResponseDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+
+//        could alternatively be:
+//        Ticket ticket = ticketService.getTicketForUser(parseUserId(jwt), ticketId)
+//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+//
+//        return ResponseEntity.ok(ticketMapper.toGetTicketResponseDto(ticket));
     }
 
 }
