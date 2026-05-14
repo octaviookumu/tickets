903ca6c5b16ac19a9b61aab11fdc8b4c34bc3b22
octaviookumu <octaviookumu@gmail.com>
2026-05-14 15:44:14 +0300

    feat: implement getQrCode endpoint

--- backend/src/main/java/com/octaviookumu/tickets/domain/controllers/TicketController.java
+++ backend/src/main/java/com/octaviookumu/tickets/domain/controllers/TicketController.java
@@ -3,10 +3,13 @@ package com.octaviookumu.tickets.domain.controllers;
 import com.octaviookumu.tickets.domain.dtos.GetTicketResponseDto;
 import com.octaviookumu.tickets.domain.dtos.ListTicketResponseDto;
 import com.octaviookumu.tickets.mappers.TicketMapper;
+import com.octaviookumu.tickets.services.QrCodeService;
 import com.octaviookumu.tickets.services.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -26,6 +29,7 @@ public class TicketController {
 
     private final TicketService ticketService;
     private final TicketMapper ticketMapper;
+    private final QrCodeService qrCodeService;
 
     @GetMapping
     public Page<ListTicketResponseDto> listTickets(
@@ -55,6 +59,24 @@ public class TicketController {
 //                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 //
 //        return ResponseEntity.ok(ticketMapper.toGetTicketResponseDto(ticket));
+    }
+
+    @GetMapping("/{ticketId}/qr-codes")
+    public ResponseEntity<byte[]> getTicketQrCode(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable UUID ticketId
+    ) {
+        byte[] qrCodeImage = qrCodeService.getQrCodeImageForUserAndTicket(parseUserId(jwt), ticketId);
+
+        // construct the headers in order to return the image correctly
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        headers.setContentLength(qrCodeImage.length);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(qrCodeImage);
+
     }
 
 }
