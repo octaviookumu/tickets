05d70f6e62a2799ba618345f05b7a452293cb6b7
octaviookumu <octaviookumu@gmail.com>
2026-05-14 15:35:16 +0300

    feat: add - findByTicketIdAndPurchaserId to QrCodeRepository - getQrCodeImageForUserAndTicket to QrCodeService

--- backend/src/main/java/com/octaviookumu/tickets/repositories/QrCodeRepository.java
+++ backend/src/main/java/com/octaviookumu/tickets/repositories/QrCodeRepository.java
@@ -4,9 +4,10 @@ import com.octaviookumu.tickets.domain.entities.QrCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
-
+    Optional<QrCode> findByTicketIdAndPurchaserId(UUID ticketId, UUID ticketPurchaserId);
 }
